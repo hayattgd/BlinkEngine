90a31a36b7de1cbe787c6ea2de16117b2205f298
hayattgd <hayattgd@gmail.com>
2026-06-15 01:29:30 +0900

    style: dont indent namespace and classes

--- engine/include/render/mesh.h
+++ engine/include/render/mesh.h
@@ -10,27 +10,27 @@
 #include "render/shader.h"
 
 namespace BlinkEngine::Engine::Render {
-  struct Vertex {
-    glm::vec3 position;
-    glm::vec3 normal;
-    glm::vec2 uv;
-  };
+struct Vertex {
+  glm::vec3 position;
+  glm::vec3 normal;
+  glm::vec2 uv;
+};
 
-  struct Texture {
-    glm::uint32 id;
-    std::string type;
-  };
+struct Texture {
+  glm::uint32 id;
+  std::string type;
+};
 
-  class Mesh {
-    public:
-      Mesh(std::vector<Vertex> vertices, std::vector<glm::uint32> indices, std::vector<Texture> textures);
-      void Draw(Render::Shader* shader);
-    private:
-      void SetupMesh();
-      std::vector<Vertex> vertices;
-      std::vector<glm::uint32> indices;
-      std::vector<Texture> textures;
+class Mesh {
+public:
+  Mesh(std::vector<Vertex> vertices, std::vector<glm::uint32> indices, std::vector<Texture> textures);
+  void Draw(Render::Shader* shader);
+private:
+  void SetupMesh();
+  std::vector<Vertex> vertices;
+  std::vector<glm::uint32> indices;
+  std::vector<Texture> textures;
 
-      glm::uint32 vao, vbo, ebo;
-  };
+  glm::uint32 vao, vbo, ebo;
+};
 }
